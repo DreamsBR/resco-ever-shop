aa95419fef1ded751233a0d4455c2bea43927d21
JostinSamuel <jostinmartinez1605@gmail.com>
2026-05-10 16:32:31 -0500

    delete product description

--- themes/sample/src/pages/productView/WhatsAppButton.tsx
+++ themes/sample/src/pages/productView/WhatsAppButton.tsx
@@ -35,6 +35,10 @@ export default function WhatsAppButton() {
           padding-top: 30px;
         }
 
+        .product__single__description {
+          display: none;
+        }
+
         .product-image {
           overflow: hidden;
         }
